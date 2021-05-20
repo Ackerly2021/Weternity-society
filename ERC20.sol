9b5b00aa9d5f42a9914aacb434ee246186ff3e97
Ackerly <84516762+Ackerly2021@users.noreply.github.com>
2021-05-20 23:11:24 +0800

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -9,8 +9,6 @@ import "./SafeMath.sol";
  *
  * @dev Implementation of the basic standard token.
  * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
- * Originally based on code by FirstBlood:
- * https://github.com/Firstbloodio/token/blob/master/smart_contract/FirstBloodToken.sol
  *
  * This implementation emits additional Approval events, allowing applications to reconstruct the allowance status for
  * all accounts just by listening to said events. Note that this isn't required by the specification, and other
