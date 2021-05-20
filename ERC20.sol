345620acfe1799735d4fef66a724fdb5da2d74db
Ackerly <84516762+Ackerly2021@users.noreply.github.com>
2021-05-20 23:11:40 +0800

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -9,7 +9,6 @@ import "./SafeMath.sol";
  *
  * @dev Implementation of the basic standard token.
  * https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20.md
- *
  * This implementation emits additional Approval events, allowing applications to reconstruct the allowance status for
  * all accounts just by listening to said events. Note that this isn't required by the specification, and other
  * compliant implementations may not do it.
